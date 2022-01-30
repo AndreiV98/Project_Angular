7dba425d4bcd91e41a87301d4d253ac2b099f672
AndreiV98 <sand.andrei@yahoo.com>
2022-01-30 18:19:14 +0200

    [commit4]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [
   {path: '', component: AppComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
+  {path: 'dashboard', component: DashboardComponent}
 ]
 
 @NgModule({
@@ -28,7 +28,8 @@ const routes: Routes = [
     AppRoutingModule,
     RouterModule.forRoot(routes),
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  userForm!: FormGroup;
+  listData: any;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) {
+      this.listData = [];
+
+      this.userForm = this.fb.group ({
+        name: ['', Validators.required],
+        email: ['', Validators.required],
+        organization: ['', Validators.required],
+      })
+   }
+
+  public addPerson() : void {
+    this.listData.push(this.userForm.value);
+    this.userForm.reset();
+  }
+
+  reset() {
+    this.userForm.reset();
+  }
+
+  remove(element: any) {
+    this.listData.forEach((value: any, index: any)=> {
+      if (value == element) {
+        this.listData.splice(index, 1);
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
